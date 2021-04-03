c15471f657295f147f79e7b1bbf1aab501781793
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2021-04-03 15:59:20 +0000

    class

--- sample.py
+++ sample.py
@@ -4,69 +4,75 @@ from netmiko import Netmiko
 from netmiko import ConnectHandler, file_transfer
 import re
 import os
-
-user_name=os.environ["BGP_MANAGER_DEVICE_USER_NAME"]
-device_type="vyos"
-agent_file_name="bgp_manager_agent.py"
-
-remote_device = {'device_type': device_type,
-                 'ip': os.environ["BGP_MANAGER_DEVICE_IP"],
-                 'username': user_name,
-                 'use_keys': True,
-                 'password': os.environ["BGP_MANAGER_DEVICE_PASSWD"],
-                 'key_file': os.environ["BGP_MANAGER_DEVICE_KEY_FILE"]}
-
-connection = Netmiko(**remote_device)
+import copy
 
 
+class VyOS:
+    def __init__(self, **kwargs):
+        self.username = kwargs["username"]
+        common = copy.copy(kwargs)
+        common["use_keys"] = True
 
-# コマンド実行
-output = connection.send_command('show ipv6 route')
-print(output)
-print("ls somewhere")
-output = connection.send_command('ls /home/vyos')
-print(output)
-print("getting pwd")
-output = connection.send_command('pwd')
-print(output)
+        self.remote_device_vyos = copy.copy(common)
+        self.remote_device_vyos["device_type"] = "vyos"
 
-connection.disconnect()
+        self.remote_device_scp = copy.copy(common)
+        self.remote_device_scp["device_type"] = "linux"
+
+        self.agent_file_name="bgp_manager_agent.py"
 
 
-source_file="bgp_manager_agent.py"
-dest_file=source_file
-file_system="/home/" + user_name + "/"
-direction="put"
+    def connect(self):
+        self.connection_vyos = Netmiko(**self.remote_device_vyos)
+        self.connection_scp  = Netmiko(**self.remote_device_scp)
 
-print("sending agent to VyOS")
-#remote_device["device_type"] = "linux_ssh"
-remote_device["device_type"] = "linux"
-connection = Netmiko(**remote_device)
-transfer_dict = file_transfer(connection,
-                              source_file=source_file, 
-                              dest_file=dest_file,
-                              file_system=file_system, 
-                              direction=direction,
-                              overwrite_file=True)
-print(transfer_dict)
-connection.disconnect()
+    def disconnect(self):
+        self.connection_vyos.disconnect()
+        self.connection_scp.disconnect()
 
-print("getting tunnel interface names")
-remote_device["device_type"] = "linux"
-connection = Netmiko(**remote_device)
-output = connection.send_command("python3 ./bgp_manager_agent.py")
+    def send_agent(self):
+        source_file=self.agent_file_name
+        dest_file=source_file
+        file_system="/home/" + self.username + "/"
+        direction="put"
+        file_transfer(self.connection_scp,
+                      source_file=source_file, 
+                      dest_file=dest_file,
+                      file_system=file_system, 
+                      direction=direction,
+                      overwrite_file=True)
 
-max_num=0
-for dev in output.split():
-    if re.match("^tun[0-9]+$", dev):
-        print(dev)
-        dev_num = int(dev[3:])
-        print(dev_num)
-        if max_num < dev_num:
-            max_num = dev_num
-
-print("max_num=" + str(max_num))
+    def max_tunnel_device_index(self):
+        connection = self.connection_vyos
+        output = connection.send_command("python3 ./bgp_manager_agent.py")
         
-    
+        max_num=-1
+        for dev in output.split():
+            if re.match("^tun[0-9]+$", dev):
+                dev_num = int(dev[3:])
+                if max_num < dev_num:
+                    max_num = dev_num
+        return max_num
 
-connection.disconnect()
+def main():
+    kwargs = {"username": os.environ["BGP_MANAGER_DEVICE_USER_NAME"],
+              "ip": os.environ["BGP_MANAGER_DEVICE_IP"],
+              "password": os.environ["BGP_MANAGER_DEVICE_PASSWD"],
+              "key_file":os.environ["BGP_MANAGER_DEVICE_KEY_FILE"]}
+    
+    vyos = VyOS(**kwargs)
+    vyos.connect()
+    
+    vyos.send_agent()
+    
+    print("getting tunnel interface names")
+    max_num = vyos.max_tunnel_device_index()
+    print("max_index = " + str(max_num))
+    
+    vyos.disconnect()
+
+if __name__ == "__main__":
+    main()
+#    test()
+
+
